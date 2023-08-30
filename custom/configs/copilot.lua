af2c425972e4e37bd912eaab0f95ea4c721b37ab
utfeight <utfeightt@gmail.com>
2023-08-30 10:50:35 +0300

    feat(configs): seperate copilot config

---
+++ custom/configs/copilot.lua
@@ -0,0 +1,47 @@
+local M = {}
+
+M.opts = {
+  panel = {
+    enabled = true,
+    auto_refresh = true, -- Refresh suggestions when typing to the buffer
+    keymap = {
+      jump_prev = "[[",
+      jump_next = "]]",
+      accept = "<CR>",
+      refresh = "gr",
+      open = "<M-CR>",
+    },
+    layout = {
+      position = "right", -- | top | left | right
+      ratio = 0.4,
+    },
+  },
+  suggestion = {
+    enabled = true,
+    auto_trigger = true,
+    debounce = 75,
+    keymap = {
+      accept = "<M-Tab>",
+      accept_word = false,
+      accept_line = false,
+      next = "<C-l>",
+      prev = "<C-h>",
+      dismiss = "<C-q>",
+    },
+  },
+  filetypes = {
+    yaml = true,
+    markdown = true,
+    help = true,
+    gitcommit = true,
+    gitrebase = true,
+    hgcommit = true,
+    svn = true,
+    cvs = true,
+    ["."] = false,
+  },
+  copilot_node_command = "node", -- Node.js version must be > 16.x
+  server_opts_overrides = {},
+}
+
+return M
