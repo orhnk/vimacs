a3fbe35b36af1754a82c3f708e4391c67dbaa884
utfeight <utfeightt@gmail.com>
2023-09-03 01:02:51 +0300

    chore(theme): change default theme (valotile to built-in theme switcher)

--- custom/chadrc.lua
+++ custom/chadrc.lua
@@ -5,8 +5,8 @@ local M = {}
 local highlights = require "custom.highlights"
 
 M.ui = {
-  theme = "gruvbox",
-  theme_toggle = { "gruvbox", "one_light" },
+  theme = "monekai",
+  theme_toggle = { "monekai", "one_light" },
 
   -- transparency = true,
 
