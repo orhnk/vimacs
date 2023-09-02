f6e1a3e54f27173e682c4cd6236575bd4a9d6077
utfeight <utfeightt@gmail.com>
2023-09-03 01:03:37 +0300

    feat(plugins): comprehensive support for github.com

--- custom/configs/octo.lua
+++ custom/configs/octo.lua
@@ -11,7 +11,126 @@ M.cmd = {
 }
 
 M.keys = {
-  -- TODO
+
+  --[[
+  gh  -> github
+  ghl -> label
+  -- gha -> actions
+  gho -> assignee
+  ghc -> card
+  ghi -> issue
+  ghj -> comment
+  -- gho -> reviewer
+  ghp -> pr
+  ghp -> repo
+  ghr -> review
+  ghs -> search
+  ght -> thread
+  ghu -> react
+  -- ghg -> Gist
+  --]]
+
+  -- stylua: ignore start
+  -- Gist
+  {"<leader>ghg", "<cmd> Octo gist list<CR>",            mode = "n", desc = "List Gists"             },
+
+  -- Actions (list)
+  {"<leader>gha", "<cmd> Octo actions<CR>",            mode = "n", desc = "List Actions"             },
+
+  -- Repo
+  {"<leader>ghpl", "<cmd> Octo repo list<CR>",            mode = "n", desc = "List Repo"              },
+  {"<leader>ghpf", "<cmd> Octo repo fork<CR>",            mode = "n", desc = "Fork Repo"              },
+  {"<leader>ghpu", "<cmd> Octo repo url<CR>",             mode = "n", desc = "Url Repo"               },
+  {"<leader>ghpv", "<cmd> Octo repo view<CR>",            mode = "n", desc = "View Repo"              },
+  {"<leader>ghpb", "<cmd> Octo repo browser<CR>",         mode = "n", desc = "Browser Repo"           },
+
+  -- React
+  {"<leader>ghuh", "<cmd> Octo reaction heart<CR>",       mode = "n", desc = "React with heart"       },
+  {"<leader>ghur", "<cmd> Octo reaction rocket<CR>",      mode = "n", desc = "React with rocket"      },
+  {"<leader>ghut", "<cmd> Octo reaction tada<CR>",        mode = "n", desc = "React with tada"        },
+  {"<leader>ghup", "<cmd> Octo reaction party<CR>",       mode = "n", desc = "React with party"       },
+  {"<leader>ghuo", "<cmd> Octo reaction hooray<CR>",      mode = "n", desc = "React with hooray"      },
+  {"<leader>ghuc", "<cmd> Octo reaction confused<CR>",    mode = "n", desc = "React with confused"    },
+  {"<leader>ghul", "<cmd> Octo reaction laugh<CR>",       mode = "n", desc = "React with laugh"       },
+  {"<leader>ghue", "<cmd> Octo reaction eyes<CR>",        mode = "n", desc = "React with eyes"        },
+  {"<leader>ghum", "<cmd> Octo reaction -1<CR>",          mode = "n", desc = "Upvote"                 },
+  {"<leader>ghup", "<cmd> Octo reaction +1<CR>",          mode = "n", desc = "Downvote"               },
+  {"<leader>ghud", "<cmd> Octo reaction thumbs_down<CR>", mode = "n", desc = "React with thumbs_down" },
+  {"<leader>ghuu", "<cmd> Octo reaction thumbs_up<CR>",   mode = "n", desc = "React with thumbs_up"   },
+
+  -- Comment
+  {"<leader>ghja", "<cmd> Octo comment add<CR>",    mode = "n", desc =    "Add Comment" },
+  {"<leader>ghjd", "<cmd> Octo comment delete<CR>", mode = "n", desc = "Delete Comment" },
+
+  -- Card
+  {"<leader>ghcm", "<cmd> Octo card move<CR>",   mode = "n", desc = "Move Card"   },
+  {"<leader>ghcr", "<cmd> Octo card remove<CR>", mode = "n", desc = "Remove Card" },
+  {"<leader>ghca", "<cmd> Octo card add<CR>",    mode = "n", desc = "Add Card"    },
+
+  -- Review
+  {"<leader>ghrc", "<cmd> Octo review close<CR>",    mode = "n", desc = "Close Review"    },
+  {"<leader>ghrs", "<cmd> Octo review start<CR>",    mode = "n", desc = "Start Review"    },
+  {"<leader>ghrd", "<cmd> Octo review discard<CR>",  mode = "n", desc = "Discard Review"  },
+  {"<leader>ghrr", "<cmd> Octo review resume<CR>",   mode = "n", desc = "Resume Review"   },
+  {"<leader>ghrs", "<cmd> Octo review submit<CR>",   mode = "n", desc = "Submit Review"   },
+  {"<leader>ghrc", "<cmd> Octo review commit<CR>",   mode = "n", desc = "Commit Review"   },
+  {"<leader>ghra", "<cmd> Octo review comments<CR>", mode = "n", desc = "comments Review" },
+
+  -- Thread
+  {"<leader>ght", "<cmd> Octo thread resolve<CR>",   mode = "n", desc = "Resolve Thread"   },
+  {"<leader>ght", "<cmd> Octo thread unresolve<CR>", mode = "n", desc = "Unresolve Thread" },
+
+  -- PR
+  {"<leader>ghns", "<cmd> Octo pr ", mode = "n", desc = "PR"     },
+  {"<leader>ghn",  "<cmd> Octo pr ", mode = "n", desc = "PR"     },
+  {"<leader>ghn",  "<cmd> Octo pr ", mode = "n", desc = "PR"     },
+  {"<leader>ghn",  "<cmd> Octo pr ", mode = "n", desc = "PR"     },
+  {"<leader>ghn",  "<cmd> Octo pr ", mode = "n", desc = "PR"     },
+  {"<leader>ghn",  "<cmd> Octo pr ", mode = "n", desc = "PR"     },
+  {"<leader>ghn",  "<cmd> Octo pr ", mode = "n", desc = "PR"     },
+  {"<leader>ghn",  "<cmd> Octo pr ", mode = "n", desc = "PR"     },
+  {"<leader>ghn",  "<cmd> Octo pr ", mode = "n", desc = "PR"     },
+  {"<leader>ghn",  "<cmd> Octo pr ", mode = "n", desc = "PR"     },
+  {"<leader>ghn",  "<cmd> Octo pr ", mode = "n", desc = "PR"     },
+  {"<leader>ghn",  "<cmd> Octo pr ", mode = "n", desc = "PR"     },
+  {"<leader>ghn",  "<cmd> Octo pr ", mode = "n", desc = "PR"     },
+  {"<leader>ghn",  "<cmd> Octo pr ", mode = "n", desc = "PR"     },
+  {"<leader>ghn",  "<cmd> Octo pr ", mode = "n", desc = "PR"     },
+  {"<leader>ghn",  "<cmd> Octo pr ", mode = "n", desc = "PR"     },
+  {"<leader>ghn",  "<cmd> Octo pr ", mode = "n", desc = "PR"     },
+  {"<leader>ghn",  "<cmd> Octo pr ", mode = "n", desc = "PR"     },
+  {"<leader>ghn",  "<cmd> Octo pr ", mode = "n", desc = "PR"     },
+
+  -- Search
+  {"<leader>ghs",   "<cmd> Octo search<CR>", mode = "n", desc = "Search Github" },
+
+  -- Assignee
+  {"<leader>ghoa", "<cmd> Octo assignee add<CR>",    mode = "n", desc = "Add Assignee"    },
+  {"<leader>ghor", "<cmd> Octo assignee remove<CR>", mode = "n", desc = "Remove Assignee" },
+
+  -- Reviewer
+  {"<leader>ghk",   "<cmd> Octo reviewer add<CR>", mode = "n", desc = "Add Code Reviewer" },
+
+  -- Issue
+  {"<leader>ghib",   "<cmd> Octo issue browser<CR>", mode = "n", desc = "Issue Browser" },
+  {"<leader>ghir",   "<cmd> Octo issue reopen<CR>", mode = "n", desc = "Reopen Issue" },
+  {"<leader>ghic",   "<cmd> Octo issue create<CR>", mode = "n", desc = "Create Issue" },
+  {"<leader>ghis",   "<cmd> Octo issue search<CR>", mode = "n", desc = "Search Issue" },
+  {"<leader>ghiu",   "<cmd> Octo issue url<CR>", mode = "n", desc = "Url of Issue" },
+  {"<leader>ghil",   "<cmd> Octo issue list<CR>", mode = "n", desc = "List Issue" },
+  {"<leader>ghie",   "<cmd> Octo issue edit<CR>", mode = "n", desc = "Edit Issue" },
+  {"<leader>ghix",   "<cmd> Octo issue close<CR>", mode = "n", desc = "Close Issue" },
+  {"<leader>ghid",   "<cmd> Octo issue reload<CR>", mode = "n", desc = "Reload Issue" },
+
+
+  -- Label
+  {"<leader>ghlc",   "<cmd> Octo label create<CR>", mode = "n", desc = "Create Label" },
+  {"<leader>ghla",   "<cmd> Octo label add<CR>", mode = "n", desc = "Add Label" },
+  {"<leader>ghlr",   "<cmd> Octo label remove<CR>", mode = "n", desc = "Remove Label" },
+
+  -- {"<leader>gh",   "<cmd> Octo<CR>", mode = "n", desc = "Github" },
+
+  -- stylua: ignore end
 }
 
 M.opts = {
