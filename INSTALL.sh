1e68422399de2faab1450b485e679549b4722d3a
utfeight <utfeightt@gmail.com>
2023-09-02 11:12:52 +0300

    feat(INSTALL): IRC Client

--- INSTALL.sh
+++ INSTALL.sh
@@ -1,3 +1,12 @@
-yay -S perf browsh
+#!/bin/sh
 
-yay -S ranger weechat
+yay -S perf browsh gh
+
+yay -S weechat # ranger is unneded (we already have an awesome file manager)
+
+yay -S nodejs-mapscii
+
+# yay -S pipx
+# pipx install textual-paint # PAINT in your terminal
+
+# gh extension install dlvhdr/gh-dash # Octo is preferred over gh dash
