215440d8293fb0ae5194ffbf7dfd458ff63f2031
utfeight <utfeightt@gmail.com>
2023-09-04 10:34:47 +0300

    build(INSTALL): discord client

--- INSTALL.sh
+++ INSTALL.sh
@@ -11,4 +11,23 @@ yay -S pipx
 # pipx install textual-paint # PAINT in your terminal
 pipx install tuir # Reddit
 
+# See: https://seniormars.github.io/posts/neomutt/#motivation
+# yay -Sy neomutt # an email client
+# yay -Sy gnupg # TODO: Still in WIP.
+
+yay -S discordo-git # Discord Client
+
+# # Create a key and follow the instructions that are prompted:
+# gpg --full-generate-key
+#
+# # Copy your public key:
+# gpg --list-secret-keys --keyid-format=long
+#
+# # FIX: Unable to open mailbox /var/spool/mail/$USER
+# sudo touch /var/mail/$USER
+# sudo chown $USER:mail /var/mail/$USER
+# sudo chmod 660 /var/mail/$USER
+
+pipx install urlscan
+
 # gh extension install dlvhdr/gh-dash # Octo is preferred over gh dash
