e394d1062be79b4eb961af9a734e35f2d6686053
utfeight <utfeightt@gmail.com>
2023-09-04 08:24:14 +0300

    feat(theme): gruvbox

--- custom/chadrc.lua
+++ custom/chadrc.lua
@@ -5,8 +5,8 @@ local M = {}
 local highlights = require "custom.highlights"
 
 M.ui = {
-  theme = "monekai",
-  theme_toggle = { "monekai", "one_light" },
+  theme = "gruvbox",
+  theme_toggle = { "gruvbox", "one_light" },
 
   -- transparency = true,
 
