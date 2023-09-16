09c697c82a328fe59cfc69f6b49b2704d0b9ebc2
utfeight <utfeightt@gmail.com>
2023-09-16 21:57:41 +0300

    chore: cleanup

--- custom/plugins.lua
+++ custom/plugins.lua
@@ -184,54 +184,6 @@ local plugins = {
     opts = require("custom.configs.cmp").opts,
   },
 
-  -- -- load luasnips + cmp related in insert mode only
-  -- {
-  --   "hrsh7th/nvim-cmp",
-  --   event = "InsertEnter",
-  --   dependencies = {
-  --     {
-  --       -- snippet plugin
-  --       "L3MON4D3/LuaSnip",
-  --       dependencies = "rafamadriz/friendly-snippets",
-  --       opts = { history = true, updateevents = "TextChanged,TextChangedI" },
-  --       config = function(_, opts)
-  --         require("plugins.configs.others").luasnip(opts)
-  --       end,
-  --     },
-  --
-  --     -- autopairing of (){}[] etc
-  --     {
-  --       "windwp/nvim-autopairs",
-  --       opts = {
-  --         fast_wrap = {},
-  --         disable_filetype = { "TelescopePrompt", "vim" },
-  --       },
-  --       config = function(_, opts)
-  --         require("nvim-autopairs").setup(opts)
-  --
-  --         -- setup cmp for autopairs
-  --         local cmp_autopairs = require "nvim-autopairs.completion.cmp"
-  --         require("cmp").event:on("confirm_done", cmp_autopairs.on_confirm_done())
-  --       end,
-  --     },
-  --
-  --     -- cmp sources plugins
-  --     {
-  --       "saadparwaiz1/cmp_luasnip",
-  --       "hrsh7th/cmp-nvim-lua",
-  --       "hrsh7th/cmp-nvim-lsp",
-  --       "hrsh7th/cmp-buffer",
-  --       "hrsh7th/cmp-path",
-  --     },
-  --   },
-  --   opts = function()
-  --     return require "plugins.configs.cmp"
-  --   end,
-  --   config = function(_, opts)
-  --     require("cmp").setup(opts)
-  --   end,
-  -- },
-
   { -- Code runner
     "Zeioth/compiler.nvim",
     keys = {
