d77148b2629691c375570d2863eab7cbb8c32cc7
utfeight <utfeightt@gmail.com>
2023-09-16 23:12:12 +0300

    feat(ext): little trick to render external ui + quit

--- custom/utils.lua
+++ custom/utils.lua
@@ -1,7 +1,12 @@
 local M = {}
 
 M.extern = function(cmd, opts)
-  require("nvterm.terminal").send(cmd, opts)
+  -- Example:
+  --  extern("vim", "vertical")
+  --  $ vim;exit # -> quit right after vim exits
+  --  so the whole UI is rendered as in terminal.
+  --  and killed when the terminal is closed.
+  require("nvterm.terminal").send(cmd .. ";exit", opts)
 end
 
 return M
