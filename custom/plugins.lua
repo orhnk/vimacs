635c1cb9a073b84a7ead54c40cf84cb31956bed7
utfeight <utfeightt@gmail.com>
2023-09-17 19:20:14 +0300

    feat(plug): undotree

--- custom/plugins.lua
+++ custom/plugins.lua
@@ -2684,6 +2684,87 @@ local plugins = {
       { "<leader>fuv", "<Cmd>UrlView vimplug<CR>", mode = "n", desc = "View Packer plugin URLs" },
     },
   },
+
+  -- {
+  --   "nvim-treesitter/nvim-treesitter-textobjects",
+  --
+  --   config = function(_, opts)
+  --     require("nvim-treesitter.configs").setup(opts)
+  --   end,
+  --
+  --   opts = {
+  --     textobjects = {
+  --       select = {
+  --         enable = true,
+  --
+  --         -- Automatically jump forward to textobj, similar to targets.vim
+  --         lookahead = true,
+  --
+  --         keymaps = {
+  --           -- You can use the capture groups defined in textobjects.scm
+  --           ["af"] = "@function.outer",
+  --           ["if"] = "@function.inner",
+  --           ["ac"] = "@class.outer",
+  --           -- You can optionally set descriptions to the mappings (used in the desc parameter of
+  --           -- nvim_buf_set_keymap) which plugins like which-key display
+  --           ["ic"] = { query = "@class.inner", desc = "Select inner part of a class region" },
+  --           -- You can also use captures from other query groups like `locals.scm`
+  --           ["as"] = { query = "@scope", query_group = "locals", desc = "Select language scope" },
+  --         },
+  --         -- You can choose the select mode (default is charwise 'v')
+  --         --
+  --         -- Can also be a function which gets passed a table with the keys
+  --         -- * query_string: eg '@function.inner'
+  --         -- * method: eg 'v' or 'o'
+  --         -- and should return the mode ('v', 'V', or '<c-v>') or a table
+  --         -- mapping query_strings to modes.
+  --         selection_modes = {
+  --           ["@parameter.outer"] = "v", -- charwise
+  --           ["@function.outer"] = "V", -- linewise
+  --           ["@class.outer"] = "<c-v>", -- blockwise
+  --         },
+  --         -- If you set this to `true` (default is `false`) then any textobject is
+  --         -- extended to include preceding or succeeding whitespace. Succeeding
+  --         -- whitespace has priority in order to act similarly to eg the built-in
+  --         -- `ap`.
+  --         --
+  --         -- Can also be a function which gets passed a table with the keys
+  --         -- * query_string: eg '@function.inner'
+  --         -- * selection_mode: eg 'v'
+  --         -- and should return true of false
+  --         include_surrounding_whitespace = true,
+  --       },
+  --
+  --       swap = {
+  --         enable = true,
+  --         swap_next = {
+  --           ["<leader>sl"] = "@parameter.inner",
+  --         },
+  --         swap_previous = {
+  --           ["<leader>sh"] = "@parameter.inner",
+  --         },
+  --       },
+  --       lsp_interop = {
+  --         enable = true,
+  --         border = "none",
+  --         floating_preview_opts = {},
+  --         peek_definition_code = {
+  --           ["<leader>df"] = "@function.outer",
+  --           ["<leader>dF"] = "@class.outer",
+  --         },
+  --       },
+  --     },
+  --   },
+  --
+  --   dependencies = "nvim-treesitter/nvim-treesitter",
+  -- },
+
+  {
+    "mbbill/undotree",
+    keys = {
+      { "<leader>fg", "<cmd> UndotreeToggle<CR>", mode = "n", desc = "View Undo Tree" },
+    },
+  },
 }
 
 return plugins
