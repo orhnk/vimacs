816c69ddd59f109a4df4108a1e9c2a5dae21be86
utfeight <utfeightt@gmail.com>
2023-09-17 19:19:27 +0300

    feat: urlview

--- custom/plugins.lua
+++ custom/plugins.lua
@@ -2663,7 +2663,27 @@ local plugins = {
         desc = "Goyo Mode",
       },
     },
-  }
+  },
+
+  {
+    "axieax/urlview.nvim",
+    cmd = "UrlView",
+
+    config = function(_, opts)
+      require("urlview").setup(opts)
+    end,
+
+    opts = {
+      -- custom configuration options --
+    },
+
+    keys = {
+      { "<leader>fuu", "<Cmd>UrlView<CR>", mode = "n", desc = "View buffer URLs" },
+      { "<leader>ful", "<Cmd>UrlView lazy<CR>", mode = "n", desc = "View Lazy URLs" },
+      { "<leader>fup", "<Cmd>UrlView packer<CR>", mode = "n", desc = "View Packer plugin URLs" },
+      { "<leader>fuv", "<Cmd>UrlView vimplug<CR>", mode = "n", desc = "View Packer plugin URLs" },
+    },
+  },
 }
 
 return plugins
