2352a3df47e635bd34396102014cdfec16217ca1
utfeight <utfeightt@gmail.com>
2023-09-17 19:21:42 +0300

    feat(plug): browser search

--- custom/plugins.lua
+++ custom/plugins.lua
@@ -2786,6 +2786,39 @@ local plugins = {
       },
     },
   },
+
+  {
+    "lalitmee/browse.nvim",
+    dependencies = { "nvim-telescope/telescope.nvim" },
+    config = function(_, opts)
+      require("browse").setup(opts)
+    end,
+
+    keys = {
+      {
+        "<leader>op",
+        function()
+          require("browse").browse()
+        end,
+      },
+    },
+    opts = {
+      -- search provider you want to use
+      provider = "google", -- duckduckgo, bing
+
+      -- either pass it here or just pass the table to the functions
+      -- see below for more
+      bookmarks = {
+        ["github"] = {
+          ["name"] = "search github from neovim",
+          ["code_search"] = "https://github.com/search?q=%s&type=code",
+          ["repo_search"] = "https://github.com/search?q=%s&type=repositories",
+          ["issues_search"] = "https://github.com/search?q=%s&type=issues",
+          ["pulls_search"] = "https://github.com/search?q=%s&type=pullrequests",
+        },
+      },
+    },
+  },
 }
 
 return plugins
