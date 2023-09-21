dab2a83c3f4477632739f534feb0cfea2a3a26d1
utfeight <utfeightt@gmail.com>
2023-09-21 17:24:05 +0300

    feat(plug): session manager

--- custom/plugins.lua
+++ custom/plugins.lua
@@ -4097,6 +4097,46 @@ local plugins = {
       { "X", mode = "v" },
     },
   },
+
+  { -- Session Manager
+    "folke/persistence.nvim",
+
+    -- event = "BufReadPre", -- this will only start session saving when an actual file was opened
+
+    -- restore the session for the current directory
+    keys = {
+      {
+        "<leader>qs",
+        function()
+          require("persistence").load()
+        end,
+        mode = "n",
+        desc = "Save Session",
+      },
+      -- restore the last session
+      {
+        "<leader>ql",
+        function()
+          require("persistence").load { last = true }
+        end,
+        mode = "n",
+        desc = "Restore Session",
+      },
+      -- stop Persistence => session won't be saved function()on exit
+      {
+        "<leader>qd",
+        function()
+          require("persistence").stop()
+        end,
+        desc = "Stop Session Save",
+        mode = "n",
+      },
+    },
+
+    opts = {
+      -- add any custom options here
+    },
+  },
 }
 
 return plugins
