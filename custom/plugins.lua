6a24abfcea22996d1029139cef2d857cabdc5f0b
utfeight <utfeightt@gmail.com>
2023-09-21 07:56:20 +0300

    feat(plug): markdowns. more like mindmaps

--- custom/plugins.lua
+++ custom/plugins.lua
@@ -4058,6 +4058,36 @@ local plugins = {
       line_blank_line_below = false, -- insert a blank line below the line
     },
   },
+
+  --  [markdown markmap]
+  --  https://github.com/Zeioth/markmap.nvim
+  {
+    "Zeioth/markmap.nvim",
+    build = "yarn global add markmap-cli",
+
+    keys = {
+      -- {"<leader>mm", "<cmd> MarkmapOpen<CR>", mode = "n", desc = "Open Markmap"},
+      -- {"<leader>mq", "<cmd> MarkmapWatchStop<CR>", mode = "n", desc = "Stop Markmap"},
+      { "<leader>cmk", "<cmd> MarkmapWatch<CR>", mode = "n", desc = "Watch Markmap" },
+      { "<leader>cms", "<cmd> MarkmapSave<CR>", mode = "n", desc = "Save Markmap" },
+    },
+
+    cmd = {
+      "MarkmapOpen",
+      "MarkmapSave",
+      "MarkmapWatch",
+      "MarkmapWatchStop",
+    },
+
+    opts = {
+      html_output = "/tmp/markmap.html", -- (default) Setting a empty string "" here means: [Current buffer path].html
+      hide_toolbar = false, -- (default)
+      grace_period = 3600000, -- (default) Stops markmap watch after 60 minutes. Set it to 0 to disable the grace_period.
+    },
+    config = function(_, opts)
+      require("markmap").setup(opts)
+    end,
+  },
 }
 
 return plugins
