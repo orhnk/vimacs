5b92893ee4d68fd3fcea0ddb4accbdae323d7461
utfeight <utfeightt@gmail.com>
2023-09-21 17:24:41 +0300

    feat(plug): plug viewers

--- custom/plugins.lua
+++ custom/plugins.lua
@@ -4137,6 +4137,56 @@ local plugins = {
       -- add any custom options here
     },
   },
+
+  -- { -- Lazy plugin installlation system
+  --   -- WARNING: Laggy, incomplete
+  --   "roobert/activate.nvim",
+  --   keys = {
+  --     {
+  --       "<leader>P",
+  --       function()
+  --         require("activate").list_plugins()
+  --       end,
+  --       desc = "Plugins",
+  --     },
+  --   },
+  -- },
+
+  {
+    "nvim-telescope/telescope.nvim",
+    keys = {
+      {
+        "<leader>P",
+        "<cmd> Telescope lazy<CR>",
+        desc = "Surf Plugins",
+      },
+    },
+
+    dependencies = {
+      "tsakirist/telescope-lazy.nvim",
+      opts = {
+        extensions = {
+          lazy = {
+            -- Optional theme (the extension doesn't set a default theme)
+            theme = "ivy",
+            -- Whether or not to show the icon in the first column
+            show_icon = true,
+            -- Mappings for the actions
+            mappings = {
+              open_in_browser = "<C-o>",
+              open_in_file_browser = "<M-b>",
+              open_in_find_files = "<C-f>",
+              open_in_live_grep = "<C-g>",
+              open_plugins_picker = "<C-b>", -- Works only after having called first another action
+              open_lazy_root_find_files = "<C-r>f",
+              open_lazy_root_live_grep = "<C-r>g",
+            },
+            -- Other telescope configuration options
+          },
+        },
+      },
+    },
+  },
 }
 
 return plugins
