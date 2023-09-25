10b8c581a11effbca52a38189255c7756232012a
utfeight <utfeightt@gmail.com>
2023-09-25 07:02:03 +0300

    feat(plug): vscode style tasks + ui

--- custom/plugins.lua
+++ custom/plugins.lua
@@ -272,20 +272,36 @@ local plugins = {
       "CompilerToggleResults",
       "CompilerRedo",
     },
+
     dependencies = {
       "stevearc/overseer.nvim",
     },
+
     opts = {},
   },
 
-  { -- The task runner for compiler.nvim
+  { -- The task runner for compiler.nvim + daily tasks
     "stevearc/overseer.nvim",
-    -- commit = "3047ede61cc1308069ad1184c0d447ebee92d749",
+    -- commit = "19aac0426710c8fc0510e54b7a6466a03a1a7377",
+
+    keys = {
+      {
+        "<leader>ra",
+        function()
+          vim.cmd [[OverseerRun]]
+          vim.cmd [[OverseerOpen]]
+        end,
+        mode = "n",
+        desc = "Run Task",
+      },
+    },
+
     cmd = {
       "CompilerOpen",
       "CompilerToggleResults",
       "CompilerRedo",
     },
+
     opts = {
       task_list = {
         direction = "bottom",
