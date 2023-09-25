7ab043804d503038d379cc8bb7138accdac99b47
utfeight <utfeightt@gmail.com>
2023-09-25 07:16:58 +0300

    feat(plug): debugpy configuration

--- custom/plugins.lua
+++ custom/plugins.lua
@@ -817,6 +817,54 @@ local plugins = {
         },
       }
 
+      dap.adapters.debugpy = {
+        -- Requires:
+        --  python -m pip install debugpy # --break-system-packages # <- if the first command doesn't work
+        type = "executable",
+        command = "python",
+        -- function()
+        --    local venv = os.getenv "VIRTUAL_ENV"
+        --    if venv then
+        --      return venv .. "/bin/python"
+        --    else
+        --      return "python" -- From $PATH
+        --    end
+        -- end,
+        args = {
+          "-m",
+          "debugpy.adapter",
+        },
+      }
+
+      -------------------------------------------------------
+      --                    DAP CONFIGS                    --
+      -------------------------------------------------------
+
+      dap.configurations.python = {
+        {
+          -- The first three options are required by nvim-dap
+          type = "debugpy", -- the type here established the link to the adapter definition: `dap.adapters.debugpy`
+          request = "launch",
+          name = "Launch file",
+          cwd = "${workspaceFolder}",
+
+          program = "${file}", -- This configuration will launch the current file if used.
+          pythonPath = function()
+            -- debugpy supports launching an application with a different interpreter then the one used to launch debugpy itself.
+            -- The code below looks for a `venv` or `.venv` folder in the current directly and uses the python within.
+            -- You could adapt this - to for example use the `VIRTUAL_ENV` environment variable.
+            local cwd = vim.fn.getcwd()
+            if vim.fn.executable(cwd .. "/venv/bin/python") == 1 then
+              return cwd .. "/venv/bin/python"
+            elseif vim.fn.executable(cwd .. "/.venv/bin/python") == 1 then
+              return cwd .. "/.venv/bin/python"
+            else
+              return "/usr/bin/python"
+            end
+          end,
+        },
+      }
+
       dap.configurations.cpp = {
         {
           name = "Launch file",
