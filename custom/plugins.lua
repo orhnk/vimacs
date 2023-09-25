b1b3301c961ce75ad12c73917bb8f21451ea623c
utfeight <utfeightt@gmail.com>
2023-09-25 07:22:13 +0300

    feat(ui): polish quickfix + loclist

--- custom/plugins.lua
+++ custom/plugins.lua
@@ -4549,6 +4549,32 @@ local plugins = {
     },
     cmd = "Nerdy",
   },
+
+  { -- Note this plugin is written as dependency of quickfix plugins
+    -- Prettier quickfix window
+    "yorickpeterse/nvim-pqf",
+
+    config = function(_, opts)
+      require("pqf").setup(opts)
+    end,
+
+    opts = {
+      signs = {
+        error = "E",
+        warning = "W",
+        info = "I",
+        hint = "H",
+      },
+
+      -- By default, only the first line of a multi line message will be shown.
+      -- When this is true, multiple lines will be shown for an entry, separated by a space
+      show_multiple_lines = false,
+
+      -- How long filenames in the quickfix are allowed to be. 0 means no limit.
+      -- Filenames above this limit will be truncated from the beginning with [...]
+      max_filename_length = 0,
+    },
+  },
 }
 
 return plugins
