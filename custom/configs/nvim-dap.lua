54e5a98d9735693e25e99fc165139e2c0d3cab6b
utfeight <utfeightt@gmail.com>
2023-09-25 07:00:49 +0300

    feat(plug): persistent breakpoints when debugging

--- custom/configs/nvim-dap.lua
+++ custom/configs/nvim-dap.lua
@@ -1,37 +1,159 @@
+-- TODO: There's a lot to do, (:h dap after lazy loading the plugin)
+local utils = require "custom.utils"
 local M = {}
 
 M.keys = {
-  { "<leader>dc", "<cmd>lua require('dap').continue()<CR>", mode = "n", desc = "Continue" },
-  { "<leader>ds", "<cmd>lua require('dap').step_over()<CR>", mode = "n", desc = "Step Over" },
-  { "<leader>di", "<cmd>lua require('dap').step_into()<CR>", mode = "n", desc = "Step Into" },
-  { "<leader>do", "<cmd>lua require('dap').step_out()<CR>", mode = "n", desc = "Step Out" },
-  { "<leader>db", "<cmd>lua require('dap').toggle_breakpoint()<CR>", mode = "n", desc = "Toggle Breakpoint" },
+  {
+    "<leader>dc",
+    "<cmd>lua require('dap').continue()<CR>",
+    mode = "n",
+    desc = "Continue",
+  },
+
+  {
+    "<leader>ds",
+    "<cmd>lua require('dap').step_over()<CR>",
+    mode = "n",
+    desc = "Step Over",
+  },
+
+  {
+    "<leader>di",
+    "<cmd>lua require('dap').step_into()<CR>",
+    mode = "n",
+    desc = "Step Into",
+  },
+
+  {
+    "<leader>do",
+    "<cmd>lua require('dap').step_out()<CR>",
+    mode = "n",
+    desc = "Step Out",
+  },
+
+  {
+    "<leader>db",
+    function()
+      -- require("persistent-breakpoints.api").toggle_breakpoint()
+      require("dap").toggle_breakpoint()
+      utils.store_breakpoints(false)
+    end, -- Persist the breakpoints
+    mode = "n",
+    desc = "Toggle Breakpoint",
+  },
+
   {
     "<leader>dB",
-    "<cmd>lua require('dap').set_breakpoint(vim.fn.input('Breakpoint condition: '))<CR>",
+    function()
+      -- require("persistent-breakpoints.api").set_conditional_breakpoint() -- Persist
+      require("dap").toggle_breakpoint(vim.fn.input "Log: ")
+      utils.store_breakpoints(false)
+    end,
     mode = "n",
-    desc = "Set Breakpoint",
+    desc = "Set Conditional Breakpoint",
   },
+
   {
-    "<leader>dl",
-    "<cmd>lua require('dap').set_breakpoint(nil, nil, vim.fn.input('Log point message: '))<CR>",
+    "<leader>dL",
+    function()
+      require("dap").toggle_breakpoint(nil, nil, vim.fn.input "Log point message: ")
+      utils.store_breakpoints(false)
+    end,
     mode = "n",
     desc = "Log Point",
   },
-  { "<leader>dl", "<cmd>lua require('dap').run_last()<CR>", mode = "n", desc = "Run Last" },
-  { "<leader>dr", "<cmd>lua require('dap').repl.open()<CR>", mode = "n", desc = "Open REPL" },
-  { "<leader>dd", "<cmd>lua require('dapui').toggle()<CR>", mode = "n", desc = "Toggle UI" },
-  { "<leader>da", "<cmd>lua require('dapui').eval()<CR>", mode = "n", desc = "Evaluate" },
-  { "<leader>du", "<cmd>lua require('dapui').scopes()<CR>", mode = "n", desc = "Scopes" },
-  { "<leader>dv", "<cmd>lua require('dapui').variables()<CR>", mode = "n", desc = "Variables" },
-  { "<leader>dw", "<cmd>lua require('dapui').watches()<CR>", mode = "n", desc = "Watches" },
+
+  {
+    "<leader>dl",
+    "<cmd>lua require('dap').run_last()<CR>",
+    mode = "n",
+    desc = "Run Last",
+  },
+
+  {
+    "<leader>dr",
+    "<cmd>lua require('dap').repl.open()<CR>",
+    mode = "n",
+    desc = "Open REPL",
+  },
+
+  {
+    "<leader>dd",
+    "<cmd>lua require('dapui').toggle()<CR>",
+    mode = "n",
+    desc = "Toggle UI",
+  },
+
+  {
+    "<leader>da",
+    "<cmd>lua require('dapui').eval()<CR>",
+    mode = "n",
+    desc = "Evaluate",
+  },
+
+  {
+    "<leader>du",
+    "<cmd>lua require('dapui').scopes()<CR>",
+    mode = "n",
+    desc = "Scopes",
+  },
+
+  {
+    "<leader>dv",
+    "<cmd>lua require('dapui').variables()<CR>",
+    mode = "n",
+    desc = "Variables",
+  },
+
+  {
+    "<leader>dw",
+    "<cmd>lua require('dapui').watches()<CR>",
+    mode = "n",
+    desc = "Watches",
+  },
+
   {
     "<leader>de",
     "<cmd>lua require('dapui').set_exception_breakpoints()<CR>",
     mode = "n",
     desc = "Exception Breakpoints",
   },
-  { "<leader>di", "<cmd>lua require('dapui').pick_one()<CR>", mode = "n", desc = "Pick One" },
+
+  {
+    "<leader>di",
+    "<cmd>lua require('dapui').pick_one()<CR>",
+    mode = "n",
+    desc = "Pick One",
+  },
+
+  { -- NOTE: Log Points are not persistent right now.
+    "<leader>dj",
+    function()
+      require("dap").toggle_breakpoint(vim.fn.input "Log: ")
+    end,
+    mode = "n",
+    desc = "Set Log Point",
+  },
+
+  {
+    "<leader>dx",
+    function()
+      -- require("persistent-breakpoints.api").clear_all_breakpoints()
+      require("dap").clear_breakpoints()
+      utils.store_breakpoints(true)
+    end,
+    mode = "n",
+    desc = "Clear Breakpoints",
+  },
+
+  {
+    "<leader>dg",
+    function()
+      require("dap").list_breakpoints(0)
+    end,
+    mode = "n",
+    desc = "Set REPL Highlight",
+  },
 }
 
 return M
--- custom/utils.lua
+++ custom/utils.lua
@@ -12,4 +12,72 @@ M.extern = function(cmd, opts)
   require("nvterm.terminal").send(cmd .. ";exit", opts)
 end
 
+HOME = os.getenv "HOME"
+
+M.store_breakpoints = function(clear)
+  -- if file doesn't exist:
+  if vim.fn.filereadable(HOME .. "/.cache/dap/breakpoints.json") == 0 then
+    -- Create file
+    os.execute("mkdir -p " .. HOME .. "/.cache/dap")
+    os.execute("touch " .. HOME .. "/.cache/dap/breakpoints.json")
+  end
+
+  local load_bps_raw = io.open(HOME .. "/.cache/dap/breakpoints.json", "r"):read "*a"
+  if load_bps_raw == "" then
+    load_bps_raw = "{}"
+  end
+
+  local bps = vim.fn.json_decode(load_bps_raw)
+  local breakpoints_by_buf = require("dap.breakpoints").get()
+  if clear then
+    for _, bufrn in ipairs(vim.api.nvim_list_bufs()) do
+      local file_path = vim.api.nvim_buf_get_name(bufrn)
+      if bps[file_path] ~= nil then
+        bps[file_path] = {}
+      end
+    end
+  else
+    for buf, buf_bps in pairs(breakpoints_by_buf) do
+      bps[vim.api.nvim_buf_get_name(buf)] = buf_bps
+    end
+  end
+  local fp = io.open(HOME .. "/.cache/dap/breakpoints.json", "w")
+  local final = vim.fn.json_encode(bps)
+  fp:write(final)
+  fp:close()
+end
+
+M.load_breakpoints = function()
+  local fp = io.open(HOME .. "/.cache/dap/breakpoints.json", "r")
+  if fp == nil then
+    print "No breakpoints found."
+    return
+  end
+  local content = fp:read "*a"
+  local bps = vim.fn.json_decode(content)
+  local loaded_buffers = {}
+  local found = false
+  for _, buf in ipairs(vim.api.nvim_list_bufs()) do
+    local file_name = vim.api.nvim_buf_get_name(buf)
+    if bps[file_name] ~= nil and bps[file_name] ~= {} then
+      found = true
+    end
+    loaded_buffers[file_name] = buf
+  end
+  if found == false then
+    return
+  end
+  for path, buf_bps in pairs(bps) do
+    for _, bp in pairs(buf_bps) do
+      local line = bp.line
+      local opts = {
+        condition = bp.condition,
+        log_message = bp.logMessage,
+        hit_condition = bp.hitCondition,
+      }
+      require("dap.breakpoints").set(opts, tonumber(loaded_buffers[path]), line)
+    end
+  end
+end
+
 return M
