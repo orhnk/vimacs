a4a9975da333d9bbc93e4fe41446af0ddb86be39
utfeight <utfeightt@gmail.com>
2023-09-28 04:41:40 +0300

    feat(info.lua): Add info.lua for crediential information

---
+++ custom/info.lua
@@ -0,0 +1,13 @@
+-- TODO: Use these in, snippets, plugins, statusline like this -> <25-09-23, utfeight>
+-- or this -> COPYRIGHT © 2021, utfeight
+
+local M = {}
+
+-- stylua: ignore start
+M.real_name = "Orhan KARAMAN"       -- Real Name
+M.email     = "utfeightt@gmail.com" -- Email
+M.github    = "utfeight"            -- Github username
+M.username  = M.github              -- Other  Sites
+-- stylua: ignore end
+
+return M
