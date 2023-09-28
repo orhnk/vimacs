9f19cc1435af80ea77c2a0e694d516065f395d2e
utfeight <utfeightt@gmail.com>
2023-09-28 04:50:05 +0300

    chore: cleanup

--- custom/configs/lspconfig.lua
+++ custom/configs/lspconfig.lua
@@ -4,7 +4,7 @@ local capabilities = require("plugins.configs.lspconfig").capabilities
 local lspconfig = require "lspconfig"
 
 -- if you just want default config for the servers then put them in a table
-local servers = { "cssls", "clangd",  "rust_analyzer", "cmake" }
+local servers = { "cssls", "clangd", "rust_analyzer", "cmake" }
 --                                    ^^^^^^^^^^^^^^^^-- TODO: Set up with rust-tools.nvim
 for _, lsp in ipairs(servers) do
   lspconfig[lsp].setup {
@@ -13,4 +13,3 @@ for _, lsp in ipairs(servers) do
   }
 end
 
--- lspconfig.pyright.setup { blabla }
